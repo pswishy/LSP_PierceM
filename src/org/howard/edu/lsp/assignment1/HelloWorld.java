ee2bbe205f632c52ed61990ed924f41641decc71
pierce <piercemedy24@gmail.com>
2023-01-26 14:31:35 -0500

    Create hello world class

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+*  Name: Pierce Medy
+*/
+
+package src.org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
