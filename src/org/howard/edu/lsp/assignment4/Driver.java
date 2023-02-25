3bb32c784ab17b2c9b4347da20f28363a3bbb6dc
pierce <piercemedy24@gmail.com>
2023-02-25 15:11:40 -0500

    fix equals method and add test

--- src/org/howard/edu/lsp/assignment4/Driver.java
+++ src/org/howard/edu/lsp/assignment4/Driver.java
@@ -1,17 +1,37 @@
 package src.org.howard.edu.lsp.assignment4;
 
 import java.util.*;
+//import static org.junit.Assert;
 
 public class Driver{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IntegerSetException {
 
         IntegerSet set1 = new IntegerSet();
+
+        // Individual Set Operations
 
         set1.add(1);
         set1.add(2);
         set1.add(3);
+        set1.add(3);
+        System.out.println("Value of Set 1 is: " + set1.toString());
+        System.out.println("Largest Value of Set 1 is: " + String.valueOf(set1.largest()));
+        System.out.println("Smallest Value of Set 1 is: " + String.valueOf(set1.smallest()));
+        System.out.println("The Length of Set 1 is: " + String.valueOf(set1.length()));
+        System.out.println("Does Set 1 contain the value 1: " + String.valueOf(set1.contains(1)));
+        System.out.println("Does Set 1 contain the value 5: " + String.valueOf(set1.contains(5)));
 
-        System.out.println(set1.toString());
+        // Set Comparison
+        IntegerSet set2 = new IntegerSet();
+        set2.add(4);
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+        System.out.println("Is Set 1 equal to Set 2: " + set1.equals(set2));
+
+        set1.union(set2);
+        System.out.println("Result of Set 1 Union With Set 2: " + set1.toString());
+
 
     }
 }
--- src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -18,35 +18,18 @@ public class IntegerSet {
     }
 
     public boolean equals(IntegerSet b){
-        Dictionary<Integer, Integer> dict1 = new Hashtable<>();
-        Dictionary<Integer, Integer> dict2 = new Hashtable<>();
-
-        for (int i: set){
-            // if this triggers num is not found
-            // so put inside dictionary
-            if(dict1.get(i) == null){
-                dict1.put(i, 1);
-            }
-            else{
-                // number is already in dictionary
-                // so update count
-                dict1.put(i, dict1.get(i) + 1);
-            }
+        Boolean equals = true;
+        
+        if(set.size() != b.set.size()){
+            return false;
         }
 
         for (int i: b.set){
-            // if this triggers num is not found
-            // so put inside dictionary
-            if(dict2.get(i) == null){
-                dict2.put(i, 1);
-            }
-            else{
-                // number is already in dictionary
-                // so update count
-                dict2.put(i, dict2.get(i) + 1);
+            if(!set.contains(i)){
+                equals = false;
             }
         }
-        return dict1 == dict2;
+        return equals;
     }
 
     public boolean contains(int value){
