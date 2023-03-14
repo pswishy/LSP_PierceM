b72a0bd5ed0e9e2dbaad595e228e8e153a9b9e96
pierce <piercemedy24@gmail.com>
2023-03-14 13:13:16 -0400

    finish testing for integerset

--- src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -8,6 +8,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.*;
 
 public class IntegerSetTest {
+
+    @Test
+    public void testClear(){
+        IntegerSet set1 = new IntegerSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.clear();
+        assertEquals(0, set1.length());
+
+        IntegerSet set2 = new IntegerSet();
+        set2.clear();
+        assertEquals(0, set2.length());
+
+    }
     
     @Test 
     public void testLength(){
@@ -132,15 +147,122 @@ public class IntegerSetTest {
         IntegerSet setA = new IntegerSet();
         setA.add(1);
         setA.add(2);
-        setA.add(2);
         setA.add(3);
-        setA.add(3);
-        assertEquals(3, setA.length());
+        setA.remove(3);
+        assertEquals(2, setA.length());
+        assertFalse(setA.contains(3));
 
         IntegerSet setB = new IntegerSet();
-        setB.add(-100);
-        setB.add(-1000);
-        setB.add(0);
-        assertEquals(3, setB.length());
+        setB.remove(0);
+        assertEquals(0, setB.length());
     }
+
+    @Test 
+    public void testUnion() {
+
+        IntegerSet setA = new IntegerSet();
+        setA.add(1);
+        setA.add(2);
+        setA.add(3);
+
+        IntegerSet setB = new IntegerSet();
+        setB.add(0);
+
+        setA.union(setB);
+        assertEquals(4, setA.length());
+        assertTrue(setA.contains(0));
+
+        IntegerSet setC = new IntegerSet();
+        IntegerSet setD = new IntegerSet();
+
+        setC.union(setD);
+
+        assertEquals(0, setC.length());
+    }
+
+    @Test 
+    public void testIntersection() {
+
+        IntegerSet setA = new IntegerSet();
+        setA.add(1);
+        setA.add(2);
+        setA.add(3);
+
+        IntegerSet setB = new IntegerSet();
+        setB.add(4);
+        setB.add(3);
+        setB.add(2);
+        setB.add(1);
+
+        setA.intersection(setB);
+        assertEquals(3, setA.length());
+        assertTrue(setA.contains(3));
+        assertFalse(setA.contains(4));
+
+    }
+
+    @Test 
+    public void testDifference() {
+
+        IntegerSet setA = new IntegerSet();
+        setA.add(5);
+        setA.add(5);
+        setA.add(5);
+
+        IntegerSet setB = new IntegerSet();
+        setB.add(3);
+        setB.add(2);
+        setB.add(1);
+        assertEquals(-1, setA.diff(setB));
+        
+        IntegerSet setC = new IntegerSet();
+        IntegerSet setD = new IntegerSet();
+
+        assertEquals(0, setC.diff(setD));
+
+        assertEquals(5, setA.diff(setD));
+
+    }
+
+    @Test 
+    public void testIsEmpty() {
+
+        IntegerSet setA = new IntegerSet();
+        setA.add(5);
+        setA.add(5);
+        setA.add(5);
+        assertFalse(setA.isEmpty());
+
+        IntegerSet setB = new IntegerSet();
+        setB.add(3);
+        setB.add(2);
+        setB.add(1);
+        assertFalse(setB.isEmpty());
+        
+        IntegerSet setC = new IntegerSet();
+        IntegerSet setD = new IntegerSet();
+
+        assertTrue(setC.isEmpty());
+        assertTrue(setD.isEmpty());
+    }
+
+    @Test 
+    public void testToString() {
+
+        IntegerSet set1 = new IntegerSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(3);
+        assertEquals("1 2 3 ", set1.toString());
+
+        IntegerSet set2 = new IntegerSet();
+
+        set2.add(10);
+        set2.add(20);
+        set2.add(30);
+        set2.add(40);
+        assertEquals("10 20 30 40 ", set2.toString());
+    }
+
 }
